4160467761af545e0be9cd1bcb7275d70a80b96e
jchong1 <jocchong@ucsc.edu>
2018-07-04 18:02:21 -0700

    adding authentication through aws-amplify-react-native

--- App.js
+++ App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { createStackNavigator } from 'react-navigation';
 
 /* Screen imports */
-import Main from './src/screens/Main';
 import Splash from './src/screens/Splash';
 
 /* AWS Imports */
 import Amplify from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native';
 import aws_exports from './src/aws-exports';
 
 Amplify.configure(aws_exports);
 
-export default class App extends React.Component {
+class App extends React.Component {
     render () {
         return <Splash />;
     }
 }
+
+export default withAuthenticator(App);
