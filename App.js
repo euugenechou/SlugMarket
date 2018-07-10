54935e7727c913b9ac74e9d279be7939c39cf8a2
jchong1 <jocchong@ucsc.edu>
2018-07-10 15:11:47 -0700

    backend working question mark?

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { createStackNavigator } from 'react-navigation';
 
 /* Screen imports */
 import Splash from './src/screens/Splash';
 
 /* AWS Imports */
 import Amplify from 'aws-amplify';
-import { withAuthenticator } from 'aws-amplify-react-native';
 import aws_exports from './src/aws-exports';
 
 Amplify.configure(aws_exports);
