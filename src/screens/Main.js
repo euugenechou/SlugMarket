df618f2318d9835fc72264bb07f24b57cab49dc7
jchong1 <jocchong@ucsc.edu>
2018-07-11 10:19:01 -0700

    uhh

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,6 +1,9 @@
 /* React imports */
-import React from 'react';
-import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
+import React from "react";
+import {
+  createBottomTabNavigator,
+  createStackNavigator
+} from "react-navigation";
 
 /* Local imports */
 import Explore from "./Explore";
@@ -10,64 +13,65 @@ import Settings from "./Settings";
 import Icon from "react-native-vector-icons/Ionicons";
 
 export default class Main extends React.Component {
-  render () {
+  render() {
     return <BottomTabNavigator />;
   }
 }
 
-const ExploreStack = createStackNavigator({
-  MainExplore: { screen: Explore },
-  AddItem: { screen: AddItem }
-})
-
-const BottomTabNavigator = createBottomTabNavigator({
-  Explore: {
-    screen: ExploreStack,
-    navigationOptions: {
-      tabBarLabel: 'EXPLORE',
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name="ios-search-outline"
-          color={tintColor}
-          size={24} />
-      )
-    }
-  },
-  Profile: {
-    screen: Profile,
-    navigationOptions: {
-      tabBarLabel: 'PROFILE',
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name="ios-person-outline"
-          color={tintColor}
-          size={24} />
-      )
-    }
-  },
-  Settings: {
-    screen: Settings,
-    navigationOptions: {
-      tabBarLabel: 'SETTINGS',
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name="ios-settings-outline"
-          color={tintColor}
-          size={24} />
-      )
-    }
-  }
-},
+const ExploreStack = createStackNavigator(
   {
-    tabBarPosition: 'bottom',
-    swipeEnabled: 'true',
+    MainExplore: { screen: Explore },
+    AddItem: { screen: AddItem }
+  },
+  {
+    mode: "modal"
+  }
+);
+
+const BottomTabNavigator = createBottomTabNavigator(
+  {
+    Explore: {
+      screen: ExploreStack,
+      navigationOptions: {
+        tabBarLabel: "EXPLORE",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="ios-search-outline" color={tintColor} size={24} />
+        )
+      }
+    },
+    Profile: {
+      screen: Profile,
+      navigationOptions: {
+        tabBarLabel: "PROFILE",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="ios-person-outline" color={tintColor} size={24} />
+        )
+      }
+    },
+    Settings: {
+      screen: Settings,
+      navigationOptions: {
+        tabBarLabel: "SETTINGS",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="ios-settings-outline" color={tintColor} size={24} />
+        )
+      }
+    }
+  },
+  {
+    tabBarPosition: "bottom",
+    swipeEnabled: "true",
     tabBarOptions: {
-      activeTintColor: 'teal',
-      inactiveTintColor: 'grey',
+      activeTintColor: "teal",
+      inactiveTintColor: "grey",
       style: {
-        backgroundColor: 'white',
+        backgroundColor: "white",
         borderTopWidth: 0,
         shadowOffset: { width: 5, height: 3 },
-        shadowColor: 'black',
+        shadowColor: "black",
         shadowOpacity: 0.5,
         elevation: 5
       }
     }
-});
+  }
+);
