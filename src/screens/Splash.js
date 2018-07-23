9f327ee38944c09a70752cd466fa85afdfb6e61a
Eugene Chou <euchou@ucsc.edu>
2018-07-22 21:08:21 -0700

    prettyfing splash

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,22 +1,27 @@
 // React imports
-import React, { Component } from 'react';
-import { StyleSheet, ImageBackground } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, ImageBackground } from "react-native";
 
-export default class Splash extends Component {	
-	render() {
-		return <ImageBackground source={require('../assets/SplashScreeniOS.png')} style={styles.container} />;
-	}
+export default class Splash extends Component {
+  render() {
+    return (
+      <ImageBackground
+        source={require("../assets/SplashScreeniOS.png")}
+        style={styles.container}
+      />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	title: {
-		fontWeight: 'bold',
-		fontSize: 18,
-		color: 'white',
-	},
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+    color: "white"
+  }
 });
