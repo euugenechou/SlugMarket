8b659d293d44ed5921b13359e5bd4c2eb90a8b68
Eugene Chou <euchou@ucsc.edu>
2018-07-22 21:04:20 -0700

    hooked up the splash screen

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createStackNavigator } from "react-navigation";
 import Amplify from "aws-amplify";
 import aws_exports from "./aws-exports";
 // Local Imports
+import Splash from "./src/screens/Splash";
 import SignIn from "./src/screens/login/SignIn";
 import SignUp from "./src/screens/login/SignUp";
 import Confirm from "./src/screens/login/Confirm";
@@ -13,35 +14,43 @@ import { BottomTabNavigator } from "./src/screens/Main";
 import { Font } from "expo";
 import Entypo from "./node_modules/@expo/vector-icons/fonts/Entypo.ttf";
 import Ionicons from "./node_modules/@expo/vector-icons/fonts/Ionicons.ttf";
-import { YellowBox } from 'react-native'
+import { YellowBox } from "react-native";
 YellowBox.ignoreWarnings([
-	'Encountered an error loading page',   
-	'Deprecation warning: moment construction falls back to js Date. This is discouraged and will be removed in upcoming major release. Please refer to http://momentjs.com/guides/#/warnings/js-date/ for more info.',
-	'Task orphaned for request ',
-	'Remote debugger is in a background tab which may cause apps to perform slowly',
-])
+  "Encountered an error loading page",
+  "Deprecation warning: moment construction falls back to js Date. This is discouraged and will be removed in upcoming major release. Please refer to http://momentjs.com/guides/#/warnings/js-date/ for more info.",
+  "Task orphaned for request ",
+  "Remote debugger is in a background tab which may cause apps to perform slowly"
+]);
 
 Amplify.configure(aws_exports);
 
 export default class App extends React.Component {
   state = {
-    fontLoaded: false
+    fontLoaded: false,
+    component: <Splash />
   };
 
   async componentWillMount() {
     try {
       await Font.loadAsync({
         Entypo,
-        Ionicons,
+        Ionicons
       });
       this.setState({ fontLoaded: true });
     } catch (error) {
       console.log(error);
     }
+    this.timeoutHandle = setTimeout(() => {
+      this.setState({ component: <LoginStack /> });
+    }, 900);
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeoutHandle);
   }
 
   render() {
-    return <LoginStack />;
+    return this.state.component;
   }
 }
 
--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -2,30 +2,9 @@
 import React, { Component } from 'react';
 import { StyleSheet, ImageBackground } from 'react-native';
 
-// Local imports
-import { YellowBox } from 'react-native'
-YellowBox.ignoreWarnings([
-	'Encountered an error loading page',    // WebView uri: result.url and url failing to load - "bloomberg suneq" https://github.com/facebook/react-native/issues/7839#issuecomment-224111608
-	'Deprecation warning: moment construction falls back to js Date. This is discouraged and will be removed in upcoming major release. Please refer to http://momentjs.com/guides/#/warnings/js-date/ for more info.',
-	'Task orphaned for request ',
-	'Remote debugger is in a background tab which may cause apps to perform slowly',
-])
-
 export default class Splash extends Component {	
-	constructor(props) {
-		super(props);
-		this.state = {
-			timePassed: false,
-		};
-		setTimeout(() => this.setState({ timePassed: true }), 1200);
-	}
-
 	render() {
-		if (!this.state.timePassed) {
-			return <ImageBackground source={require('../assets/SplashScreeniOS.png')} style={styles.container} />;
-		} else {
-			return <Login />;
-		}
+		return <ImageBackground source={require('../assets/SplashScreeniOS.png')} style={styles.container} />;
 	}
 }
 
